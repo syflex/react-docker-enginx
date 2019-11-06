243228216d69809b810f20dd396f8f419192d295
Simon Moses <syflex360@gmail.com>
2019-11-06 19:52:18 +0100

    first commit

---
+++ build/precache-manifest.f0cddcbfc32ddcea4bca27d7abd9c980.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe8600d409997bf82ae3026df9c4efd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b85ee1d6c5979cdebe5",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "0d8084d5ae1904e5240b",
+    "url": "/static/js/2.b073f863.chunk.js"
+  },
+  {
+    "revision": "5b85ee1d6c5979cdebe5",
+    "url": "/static/js/main.66b85325.chunk.js"
+  },
+  {
+    "revision": "d653c3e1ff0c4fc20192",
+    "url": "/static/js/runtime-main.d01d61ca.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
